892e68a3327fa9a2bc28795256516d34ef5b96c1
JunHwan Kim <nefertiri972@gmail.com>
2022-07-09 23:42:55 +0900

    Add Hello World Controller

---
+++ src/main/java/com/example/restfulwebservice/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.example.restfulwebservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    // GET
+    // /hello-world (endpoint)
+    //@RequestMapping(method = RequestMethod.GET, path = "/hello-world")
+    @GetMapping("/hello-world")
+    public String helloWorld() {
+        return "Hello World";
+    }
+}
