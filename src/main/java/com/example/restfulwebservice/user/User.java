216606e6674947198fa9b005ade4c36ddc2281bb
JunHwan Kim <nefertiri972@gmail.com>
2022-07-23 18:00:24 +0900

    Add REST API version using URI
    - URI를 사용하여 REST API 버전 관리 추가

--- src/main/java/com/example/restfulwebservice/user/User.java
+++ src/main/java/com/example/restfulwebservice/user/User.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
@@ -14,6 +15,7 @@ import java.util.Date;
 //@JsonIgnoreProperties({"password", "ssn"})
 @JsonFilter("UserInfo")
 @AllArgsConstructor
+@NoArgsConstructor
 public class User {
     private Integer id;
 
