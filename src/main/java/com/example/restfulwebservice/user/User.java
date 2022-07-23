df542ae9e4ad422c82ef9996d841bd1f9e9194cd
JunHwan Kim <nefertiri972@gmail.com>
2022-07-23 16:31:01 +0900

    Add response data filtering
    - @JsonIgnore, @JsonIgnoreProperties를 사용하여 Response 데이터 필터링 추가

--- src/main/java/com/example/restfulwebservice/user/User.java
+++ src/main/java/com/example/restfulwebservice/user/User.java
@@ -1,5 +1,7 @@
 package com.example.restfulwebservice.user;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -8,6 +10,7 @@ import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Data
+@JsonIgnoreProperties({"password", "ssn"})
 @AllArgsConstructor
 public class User {
     private Integer id;
@@ -17,4 +20,10 @@ public class User {
 
     @Past
     private Date joinDate;
+
+    //@JsonIgnore
+    private String password;
+
+    //@JsonIgnore
+    private String ssn;
 }
