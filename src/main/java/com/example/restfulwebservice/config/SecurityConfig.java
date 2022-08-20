5c9100a4ea698a4908b1e177eb26284b1defa134
JunHwan Kim <nefertiri972@gmail.com>
2022-08-20 19:59:47 +0900

    Add spring security configuration
    - Configuration 클래스를 이용한 사용자 인증 처리

---
+++ src/main/java/com/example/restfulwebservice/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.example.restfulwebservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("kenneth")
+                .password("{noop}test1234")
+                .roles("USER")
+                .build());
+        return manager;
+    }
+}
