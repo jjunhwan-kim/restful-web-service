7ae38314731dd467cfed27890f6d87f7a77b9b9c
JunHwan Kim <nefertiri972@gmail.com>
2022-08-20 22:59:01 +0900

    Add user delete, create API
    - JPA를 이용한 사용자 추가와 삭제 - POST/DELETE HTTP Method

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
 
-insert into user values(1, sysdate(), 'User1', 'test1111', '701010-1111111');
-insert into user values(2, sysdate(), 'User2', 'test2222', '801010-2222222');
-insert into user values(3, sysdate(), 'User3', 'test3333', '901010-1111111');
+insert into user(id, join_date, name, password, ssn) values(10000, sysdate(), 'User1', 'test1111', '701010-1111111');
+insert into user(id, join_date, name, password, ssn) values(10002, sysdate(), 'User2', 'test2222', '801010-2222222');
+insert into user(id, join_date, name, password, ssn) values(10003, sysdate(), 'User3', 'test3333', '901010-1111111');
