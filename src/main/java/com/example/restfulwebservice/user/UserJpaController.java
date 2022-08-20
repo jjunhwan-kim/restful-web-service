3f93121db7b6e128abba106c27767a66385091f7
JunHwan Kim <nefertiri972@gmail.com>
2022-08-20 22:22:46 +0900

    Add user retrieve API
    - JPA를 이용한 사용자 목록 조회 - GET HTTP Method

--- src/main/java/com/example/restfulwebservice/user/UserJpaController.java
+++ src/main/java/com/example/restfulwebservice/user/UserJpaController.java
@@ -1,11 +1,18 @@
 package com.example.restfulwebservice.user;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RequiredArgsConstructor
 @RequestMapping("/jpa")
@@ -18,5 +25,21 @@ public class UserJpaController {
     @GetMapping("/users")
     public List<User> retrieveAllUsers() {
         return userRepository.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public EntityModel<User> retrieveUser(@PathVariable Integer id) {
+        Optional<User> user = userRepository.findById(id);
+
+        if (user.isEmpty()) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+
+        // HATEOAS
+        EntityModel<User> resource = EntityModel.of(user.get());
+        WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        resource.add(linkTo.withRel("all-user"));
+
+        return resource;
     }
 }
